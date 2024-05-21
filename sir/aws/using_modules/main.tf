b80347ce04c5ed577811791978ea5cd4a2651488
barma <barma@gmail.com>
2024-05-21 16:21:46 +0530

    added terraform

---
+++ sir/aws/using_modules/main.tf
@@ -0,0 +1,100 @@
+module "vpc" {
+  source = "github.com/asquarezone/TerraformZone//May24/aws/modules/vpc"
+  network_info = {
+    name = "ntier-primary"
+    cidr = "10.0.0.0/16"
+  }
+  public_subnets = [{
+    name = "web-1"
+    az   = "ap-south-1a"
+    cidr = "10.0.0.0/24"
+  }]
+  private_subnets = [{
+    name = "db-1"
+    az   = "ap-south-1a"
+    cidr = "10.0.50.0/24"
+    }, {
+    name = "db-2"
+    az   = "ap-south-1b"
+    cidr = "10.0.51.0/24"
+  }]
+}
+
+# create web security group
+module "web_security_group" {
+  source = "github.com/asquarezone/TerraformZone//May24/aws/modules/security_group"
+  security_group_info = {
+    name        = "web-sg"
+    description = "web security group"
+    vpc_id      = module.vpc.vpcid
+    inbound_rules = [{
+      cidr        = "0.0.0.0/0"
+      port        = 22
+      protocol    = "tcp"
+      description = "open ssh"
+      },
+      {
+        cidr        = "0.0.0.0/0"
+        port        = 80
+        protocol    = "tcp"
+        description = "open http"
+      }
+    ]
+    outbound_rules   = []
+    allow_all_egress = true
+  }
+
+  depends_on = [module.vpc]
+
+}
+
+# create a db security group to open 3306 port with in vpc
+module "db_security_group" {
+  source = "github.com/asquarezone/TerraformZone//May24/aws/modules/security_group"
+  security_group_info = {
+    name             = "mysql-db-sg"
+    description      = "open mysql port within vpc"
+    vpc_id           = module.vpc.vpcid
+    allow_all_egress = true
+    outbound_rules   = []
+    inbound_rules = [{
+      cidr        = "10.0.0.0/16"
+      port        = 3306
+      protocol    = "tcp"
+      description = "open mysql port"
+    }]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+module "instances" {
+  source = "github.com/asquarezone/TerraformZone//May24/aws/modules/ec2"
+  count  = length(var.web_instances)
+  instance_info = {
+    name                        = var.web_instances[count.index]
+    ami                         = data.aws_ami.ubuntu.id
+    instance_type               = "t2.micro"
+    key_name                    = "my_idrsa"
+    subnet_id                   = module.vpc.public_subnets[0]
+    associate_public_ip_address = true
+    security_group_id           = module.web_security_group.security_group_id
+    user_data                   = true
+    user_data_file              = "install.sh"
+    size                        = "t2.micro"
+  }
+}
---
+++ sir/azure/multi-region-ntier/dev.tfvars
@@ -0,0 +1,35 @@
+# assigning variable values
+primary_network_cidr = ["192.168.0.0/16"]
+subnet_names         = ["web", "app", "db"]
+subnet_cidrs         = ["192.168.0.0/24", "192.168.1.0/24", "192.168.2.0/24"]
+web_nsg_rules = [{
+  name                       = "openhttp"
+  description                = "this opens 80 port"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "80"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+  access                     = "Allow"
+  priority                   = 1000
+  }, {
+  name                       = "openssh"
+  description                = "this opens 22 port"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "22"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+  access                     = "Allow"
+  priority                   = 1010
+}]
+web_vm_info = {
+  name            = "webvm"
+  size            = "Standard_B1s"
+  username        = "Dell"
+  public_key_path = "~/.ssh/id_rsa.pub"
+  publisher       = "canonical"
+  offer           = "0001-com-ubuntu-server-jammy"
+  sku             = "22_04-lts-gen2"
+  version         = "latest"
+}
